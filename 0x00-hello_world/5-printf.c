3d1a1677580123fc9e2b5e62e9a0b57de648aecd
seafemam-code <seafemam@gmail.com>
2023-02-16 10:45:24 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#includ <stdio.h>
+#include <stdio.h>
 /**
- * main - A program that print a line using the printf function
- * Return: 0 (Success)
- */ 
-
-int  main(void)
+ * main - print a c program using printf
+ *
+ * Return: 0
+*/
+int main(void)
 {
-     printf("with proper grammar, but the outcome is a piece of art,\n);
-     return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
