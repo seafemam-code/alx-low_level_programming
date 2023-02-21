8f89e2e9fcda31e23f15be812063e8488d76cfdb
seafemam-code <seafemam@gmail.com>
2023-02-21 11:21:23 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,9 +6,7 @@
  */
 
 int main(void)
-
 {
-        
 	_putchar('_');
         _putchar('p');
         _putchar('u');
@@ -17,6 +15,6 @@ int main(void)
         _putchar('h');
         _putchar('a');
         _putchar('r');
-        _putchar('\n');
-        return (0);
+	_putchar('\n');
+	return (0);
 }
