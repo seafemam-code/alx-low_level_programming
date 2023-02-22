4d7ff4741afe5e50beca6f37479a46a24f0dae09
seafemam-code <seafemam@gmail.com>
2023-02-22 07:03:38 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n;
+	n = add(98, 9);
+	printf("%d\n", n);
+	return (0);
+}
