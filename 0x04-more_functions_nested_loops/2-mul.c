22925ccf2074a242a283c0542fd82581f3243f66
seafemam-code <seafemam@gmail.com>
2023-02-24 06:47:26 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integer
+ * @x: The first number to multiply
+ * @y: The second number to multiply with first number
+ * Return: The result of multiplication between x paran and y paran
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
